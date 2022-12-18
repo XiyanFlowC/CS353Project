3e4226436b1864c44fa65229eb9f6b428ea8a5b6
XiyanFlowC <1197129380@qq.com>
2022-12-18 14:48:21 +0800

    Fixed incorrect notation.

--- meowtool/src/main/java/org/cstp/meowtool/api/proj/term/TerminologyController.java
+++ meowtool/src/main/java/org/cstp/meowtool/api/proj/term/TerminologyController.java
@@ -1,7 +1,5 @@
 package org.cstp.meowtool.api.proj.term;
 
-import javax.websocket.server.PathParam;
-
 import org.cstp.meowtool.database.Terminology;
 import org.cstp.meowtool.database.TerminologyMapper;
 import org.cstp.meowtool.utils.AuthUtil;
@@ -9,6 +7,7 @@ import org.cstp.meowtool.utils.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,13 +28,13 @@ public class TerminologyController {
     
     @ApiOperation("Select a terminology by ID")
     @GetMapping("/{id}")
-    public Result selectTerminology(@PathParam("id") Integer id) {
+    public Result selectTerminology(@PathVariable("id") Integer id) {
         return Result.succ(terminologyMapper.selectTerminology(id));
     }
 
     @ApiOperation("Update a terminology by ID")
     @PutMapping("/{id}")
-    public Result updateTerminology(@PathParam("id") Integer id, @RequestBody Terminology data) {
+    public Result updateTerminology(@PathVariable("id") Integer id, @RequestBody Terminology data) {
         if (!authUtil.hasProjectRole(data.getProjId(), "SUPERVISOR")) return Result.fail(-105, "Permission denied.");
 
         data.setCommiter(authUtil.getUser().getId());
@@ -45,7 +44,7 @@ public class TerminologyController {
 
     @ApiOperation("Delete a terminology by ID")
     @DeleteMapping("/{id}")
-    public Result deleteTerminology(@PathParam("id") Integer id) {
+    public Result deleteTerminology(@PathVariable("id") Integer id) {
         if (!authUtil.hasProjectRole(id, "SUPERVISOR")) return Result.fail(-105, "Permission denied.");
 
         return Result.succ(terminologyMapper.deleteTerminology(id));
